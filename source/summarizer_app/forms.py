20ed1a7b5d9deb569b7ffa9c439c6cbfa4d6ca46
capiedrav <capiedrav@gmail.com>
2025-05-19 16:09:06 -0500

    improved responsive design of the form

--- source/summarizer_app/forms.py
+++ source/summarizer_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, Submit, Field
+from crispy_forms.layout import Layout, Fieldset, Submit, Field, Div
 
  
 youtube_url_validator = RegexValidator(
@@ -20,18 +20,17 @@ class YoutubeUrlForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "post"
-        self.helper.form_action = "home"        
+        self.helper.form_action = "home"
+        self.helper.form_class = "d-flex flex-column align-items-md-start align-items-center"
         self.helper.layout = Layout(
-            Fieldset(
-                "",
-                Field(
-                    "url", 
-                    css_class="form-control form-control-lg",
-                    placeholder="https://www.youtube.com/watch?v=...",
-                    id="youtubeUrl",
-                    pattern="^(https?://)?(www\.)?(youtube\.com|youtu\.be)/(watch\?v=)([a-z|A-Z|0-9]+$)",
-                    required="true"
-                )
-                ),
-            Submit(name="submit", value="Submit")
+            Field(
+                "url",
+                css_class="form-control shadow",
+                wrapper_class="align-self-stretch",
+                placeholder="https://www.youtube.com/watch?v=...",
+                id="youtubeUrl",
+                pattern="^(https?://)?(www\.)?(youtube\.com|youtu\.be)/(watch\?v=)([a-z|A-Z|0-9]+$)",
+                required="true"
+            ),
+            Submit(name="submit", value="Summarize Video")
         )
