ca1054769ed595a2a36262efc587c252ba31a3c5
capiedrav <capiedrav@gmail.com>
2025-06-26 21:40:43 -0500

    working in test_forms.py

--- source/summarizer_app/tests/test_forms.py
+++ source/summarizer_app/tests/test_forms.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from summarizer_app.forms import YoutubeUrlForm
+from unittest.mock import patch
+from django_recaptcha.client import RecaptchaResponse
 
 
 class YoutubeURLFormTests(TestCase):
@@ -7,12 +9,20 @@ class YoutubeURLFormTests(TestCase):
     Tests for Youtube url form.
     """
 
-    def test_form_validates_youtube_url(self):
+    @patch("django_recaptcha.fields.client.submit")
+    def test_form_validates_youtube_url(self, mocked_submit):
+
+        mocked_submit.return_value = RecaptchaResponse(
+            is_valid=True,
+            extra_data={"score": 0.9},
+            action="youtube_url_form"
+        )
 
         test_url = "https://www.youtube.com/watch?v=5bId3N7QZec"
-        form = YoutubeUrlForm(data={"url": test_url})
+        form = YoutubeUrlForm(data={"url": test_url, "captcha": "PASSED"})
         
         self.assertTrue(form.is_valid())
+        mocked_submit.assert_called_once()
         self.assertEqual(form.cleaned_data["url"], test_url)
 
     def test_form_not_valid_on_wrong_url(self):
