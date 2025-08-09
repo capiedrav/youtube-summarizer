7b882bf2e676320ce033cbaa6ef275b2422c567f
capiedrav <capiedrav@gmail.com>
2025-08-08 20:10:36 -0500

    change default value for title field in YTSummary model

--- source/summarizer_app/models.py
+++ source/summarizer_app/models.py
@@ -6,7 +6,7 @@ class YTSummary(models.Model):
 
     video_id = models.CharField(max_length=22, primary_key=True, default=None, null=False)
     url = models.URLField(validators=[youtube_url_validator, ], default=None, null=False)
-    title = models.CharField(max_length=255, default="N/A", null=False)
+    title = models.CharField(max_length=255, default="Title Not Available", null=False)
     thumbnail = models.ImageField(default="thumbnails/thumbnail_not_found.jpg", null=False) # this path is relative to
                                                                                             # MEDIA_ROOT in settings.py
     video_text = models.TextField(default=None, null=False)
