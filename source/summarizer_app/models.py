65cf29a61a7830e0ca5358b1f12d0bac2d93ad76
capiedrav <capiedrav@gmail.com>
2025-09-01 11:25:30 -0500

    commits cherry-picked from development branch:
    	* 526048c add created_on field in video summary page
    	* 390302d video summary list implemented
    	* 350d944 change auto_now to auto_now_add in created_on field of YTSummary
    	* 9b58c46 register YTSummary model in admin app
    	* 4ec84ba working in video summary list page
    	* d1d42bd change default value for title field in YTSummary model. Include migration file

--- source/summarizer_app/models.py
+++ source/summarizer_app/models.py
@@ -1,23 +1,27 @@
 from django.db import models
 from .forms import youtube_url_validator
+from django.urls import reverse
 
 
 class YTSummary(models.Model):
 
     video_id = models.CharField(max_length=22, primary_key=True, default=None, null=False)
     url = models.URLField(validators=[youtube_url_validator, ], default=None, null=False)
-    title = models.CharField(max_length=255, default="N/A", null=False)
+    title = models.CharField(max_length=255, default="Title Not Available", null=False)
     thumbnail = models.ImageField(default="thumbnails/thumbnail_not_found.jpg", null=False) # this path is relative to
                                                                                             # MEDIA_ROOT in settings.py
     video_text = models.TextField(default=None, null=False)
     video_summary = models.TextField(default=None, null=False)    
-    created_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = "YTSummaries"
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(viewname="video-summary", kwargs={"pk": self.video_id})
 
     def save(self, force_insert = ..., force_update = ..., using = ..., update_fields = ...,):
         """
