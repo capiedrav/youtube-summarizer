a4bd5be1181784152da852784dd5e34bf522f57f
capiedrav <68975510+capiedrav@users.noreply.github.com>
2026-07-11 20:48:32 -0500

    refactor: debug cfn templates - part 2

--- cloudformation/init_script.sh
+++ cloudformation/init_script.sh
@@ -4,10 +4,30 @@ set -e
 STACK_NAME=$1
 REGION=$2
 
-# 1. Update and install cfn-bootstrap tools
+# 1. Update and install additional tools
 apt-get update -y
-apt-get install -y python3-pip
-pip3 install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz
+apt-get install -y rsync wget
+
+# Install AWS SSM Agent (to allow connection to the ec2 instance via ssm)
+mkdir -p /tmp/ssm
+cd /tmp/ssm
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+dpkg -i amazon-ssm-agent.deb
+systemctl enable amazon-ssm-agent
+systemctl start amazon-ssm-agent
+
+# Install pip and the virtual environment module
+apt-get install -y python3-pip python3-venv
+
+# Create a python virtual environment for AWS tools
+python3 -m venv /opt/aws-cfn-venv
+
+# Install cfn-bootstrap safely inside the isolated environment
+/opt/aws-cfn-venv/bin/pip install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz
+
+# Symlink the binary so the default paths (/usr/local/bin/cfn-init and /usr/local/bin/cfn-signal) work seamlessly
+ln -sf /opt/aws-cfn-venv/bin/cfn-init /usr/local/bin/cfn-init
+ln -sf /opt/aws-cfn-venv/bin/cfn-signal /usr/local/bin/cfn-signal
 
 # 2. Signal cfn-init to process metadata packages (git, curl, etc.)
 /usr/local/bin/cfn-init -v --stack "${STACK_NAME}" --resource DebianInstance --region "${REGION}"
@@ -38,6 +58,9 @@ apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl enable docker
 systemctl start docker
+
+# Add the default Debian admin user to the docker group
+usermod -aG docker admin
 
 # 6. Signal back to CloudFormation instance resource directly
 /usr/local/bin/cfn-signal -e $? --stack "${STACK_NAME}" --resource DebianInstance --region "${REGION}"
